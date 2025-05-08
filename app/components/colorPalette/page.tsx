26d817c5a293520cb837d9bd3d22c77b7712faa3
dauntflash <ndwiga14053@student.mmarau.ac.ke>
2025-05-08 21:03:44 +0300

    add 'use client' directive to ColorPalete component and clean up globals.css

--- app/components/colorPalette/page.tsx
+++ app/components/colorPalette/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useRef, useState, useEffect } from "react";
 import { toast } from "sonner";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
